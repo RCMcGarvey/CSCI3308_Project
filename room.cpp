34766e4961ea1c74509d4699fc49b82ec44b9bb0
Kelley Kelley <mike1555@colorado.edu>
2019-04-01 19:15:28 -0600

    Add files via upload

--- room.cpp
+++ room.cpp
@@ -1,8 +1,62 @@
 #include "room.h"
+#include <QString>
 
 //Kelley
 
+//call this after they move, pass it 1 for north, 2 for east
+//3 for south, 4 for west, 5 for up a floor/level
+QString room::nextRoom(int dir)
+{
+    switch(dir)
+    {
+    case 1://north
+        y++;
+        if(y > 4) {
+            y = 4;
+            return cant;
+        }
+        break;
+    case 2://east
+        x++;
+        if(x > 4) {
+            x = 4;
+            return cant;
+        }
+        break;
+    case 3://south
+        y--;
+        if(y < 0) {
+            y = 0;
+            return cant;
+        }
+        break;
+    case 4://west
+        x--;
+        if(x < 0) {
+            x = 0;
+            return cant;
+        }
+        break;
+    case 5://up
+        if(stairs[x][y][z]) {
+            z++;
+        } else {
+            return cant;
+        }
+        break;
+    default:
+        break;
+    }
+    return where[x][y][z];
+}
+
 room::room()
 {
-
+    int i;
+    int f;
+    for(i = 0; i < 5; i++) {
+        for(f = 0; f < 5; f++) {
+            where[i][f][0] = "Nothing in here yet";
+        }
+    }
 }
--- room.h
+++ room.h
@@ -1,11 +1,17 @@
 #ifndef ROOM_H
 #define ROOM_H
-
+#include <QString>
 
 class room
 {
 public:
     room();
+    QString nextRoom(int dir);
+    QString where[5][5][2];
+    bool locked[5][5][2];
+    bool stairs[5][5][2];
+    int x = 0, y = 0, z = 0;
+    QString cant = "You can't go that way.";
 };
 
 #endif // ROOM_H
