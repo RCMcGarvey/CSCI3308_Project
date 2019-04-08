1255a4244d8b970fef8bce265b36abe7d442236e
njxnes <47005161+njxnes@users.noreply.github.com>
2019-04-08 17:01:25 -0600

    Added enemy class framework

--- enemy.h
+++ enemy.h
@@ -1,11 +1,60 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 
+enum EnemyType{Ghost, Zombie, Skeleton}; // Base enemy subtypes
 
-class enemy
-{
-public:
-    enemy();
+/* Enemy attribute list:
+
+struct Ghost {
+
+    int health = 100;
+    int max_health = 100;
+    int base_attack = 5;
+    double crit_chance = 0.1;
+
+};
+
+struct Zombie {
+
+    int health = 150;
+    int max_health = 150;
+    int base_attack = 7;
+    double crit_chance = 0.05;
+
+};
+
+struct Skeleton {
+
+    int health = 60;
+    int max_health = 75;
+    int base_attack = 10;
+    double crit_chance = 0.3;
+
+};
+
+*/
+
+class enemy {
+
+    public:
+
+        enemy(); // Default constructor
+        enemy(EnemyType, int, int, int, double); // Enemy type, health, max_health, base_attack, crit_chance
+        enemy(EnemyType); // Set attributes based only on enemy type (easier?)
+        ~enemy();
+
+        void adjustHealth(int); // For taking damage/healing
+        int attack(); // For dealing damage
+
+    private:
+
+        EnemyType type;
+        int health;
+        int max_health;
+        int base_attack;
+        bool alive;
+        double crit_chance;
+
 };
 
 #endif // ENEMY_H
--- player.h
+++ player.h
@@ -1,12 +1,83 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+/*
 
 
+*/
 
-class player
-{
-public:
-    player();
+#include <string>
+#include "enemy.h"
+
+//Feel free to add other item types here
+enum class ItemType{Weapon, Armor, Spell, Other};
+
+//Base item class (may be subclassed for different behaviors or effects)
+struct Item{
+
+  //what the item is
+  ItemType type;
+  std::string name;
+  //item behavior when
+  bool is_single_use;
+  bool is_active;
+  // int uses; //how many times item can be used
+  int attack_boost;
+  int defense_boost;
+  double crit_chance;
+
+// in case the item is a sheet of paper which says something
+  bool has_message;
+  std::string message;
+
+  //Constructor: (basic weapons, armor, and potions)
+  //  type        name     singleuse atk dfs  crit
+  Item(ItemType, std::string, bool, int, int, double); //set
+
+  //Constructor: (scrolls, items with messages, misc)
+  //  type        name     singleuse atk dfs  crit    use/message
+  Item(ItemType, std::string, bool, int, int, double, std::string);
+
+  //open to implementation
+  void display_message();
+  // bool action();//may be used to destroy single-use items after use
 };
 
+//basic player class
+class Player{
+public:
+  Item* inventory[8];
+  //[Weapon, Armor, Spell, item, item, item, item, item]
+  int health;//current health
+  int max_health;//can change as character levels up
+  int base_attack;
+  // double defense;
+  int attack_boost;
+  int defense_boost;
+
+  bool in_combat;
+
+  Player();
+  Player(int);
+
+  //tries to add an item to the inventory
+  //returns false if item cannot be collected
+  bool collectItem(Item*);
+  //Clears the <param>th inventory slot
+  Item* dropItem(int);
+  //for taking damage or recovering health
+  void adjustHealth(int);
+
+  //Activates or deactivates a certain item in the inventory
+  void toggleItem(int slot);
+
+  //Starts a player/npc combat event
+  int combatEvent(enemy*);
+  //Ends a combat event
+  void endCombat();
+
+  //returns attack damage
+  int attack();
+  //Updates player's stats fields
+  void calculateStats();
+};
 #endif // PLAYER_H
