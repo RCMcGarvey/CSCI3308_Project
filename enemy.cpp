4efe398a18006e7993ab17d88405a09feb903b17
njxnes <47005161+njxnes@users.noreply.github.com>
2019-04-12 17:08:46 -0600

    Enemy subtypes additions
    + Added two more enemy subtypes: Vampire, Ghoul
    + Added special enemy abilities

--- enemy.cpp
+++ enemy.cpp
@@ -17,7 +17,7 @@ enemy::enemy() { // Create default enemy
 
 }
 
-enemy::enemy(EnemyType t, int h, int h_max, int b_atk, double c) { // Enemy type, health, max_health, base_attack
+enemy::enemy(EnemyType t, int h, int h_max, int b_atk, double c) { // Enemy type, health, max_health, base_attack, crit_chance
 
     type = t;
     health = h;
@@ -51,6 +51,12 @@ enemy::enemy(EnemyType t) { // Set attributes based on enemy type
             base_attack = 10;
             crit_chance = 0.3;
             break;
+        case Vampire:
+            health = 80;
+            max_health = 80;
+            base_attack = 7;
+            crit_chance = 0.3;
+            break;
     }
     alive = true;
 }
@@ -75,12 +81,26 @@ int enemy::attack() {
     int atk = base_attack;
     int bonus = rand() % 1000;
     (crit_chance * 1000 > bonus) ? atk *= 3 : atk *= 1;
+    if (type == Vampire) { // Vampire type steals health
+        adjustHealth(atk / 2);
+    }
+    if (type == Ghoul) { // Ghoul type regens health each turn
+        adjustHealth(5);
+    }
     return atk;
 
 }
 
-//int main() {
+bool enemy::getAlive() {
 
-//    srand(time(0)); // Seeded 4/5
+    return alive;
 
-//}
+}
+
+/*
+int main() {
+
+    srand(time(0)); // Seeded 4/5
+
+}
+*/
--- enemy.h
+++ enemy.h
@@ -1,27 +1,56 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 
-enum EnemyType{Ghost, Zombie, Skeleton}; // Base enemy subtypes
+enum EnemyType{Ghost, Zombie, Skeleton, Vampire, Ghoul}; // Base enemy subtypes
 
 /* Enemy attribute list:
-struct Ghost {
+
+struct Ghost { // Basic enemy
+
     int health = 100;
     int max_health = 100;
     int base_attack = 5;
     double crit_chance = 0.1;
+
 };
-struct Zombie {
+
+struct Zombie { // Basic enemy, tanky
+
     int health = 150;
     int max_health = 150;
     int base_attack = 7;
     double crit_chance = 0.05;
+
 };
-struct Skeleton {
+
+struct Skeleton { // Basic enemy, glass cannon
+
     int health = 60;
     int max_health = 75;
     int base_attack = 10;
     double crit_chance = 0.3;
+
 };
+
+struct Vampire { // Health stealer
+
+    int health = 80;
+    int max_health = 80;
+    int base_attack = 7;
+    double crit_chance = 0.3;
+
+};
+
+struct Ghoul { // More annoying, regenerative variant of Ghost type
+
+    int health = 100;
+    int max_health = 100;
+    int base_attack = 5;
+    double crit_chance = 0.4
+
+};
+
+
 */
 
 class enemy {
@@ -35,7 +64,8 @@ class enemy {
 
         void adjustHealth(int); // For taking damage/healing
         int attack(); // For dealing damage
-        bool getAlive(){return alive;}
+        bool getAlive();
+
     private:
 
         EnemyType type;
