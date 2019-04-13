a430abb6e39c43c2703d62e8ded5bb8d8128e009
njxnes <47005161+njxnes@users.noreply.github.com>
2019-04-12 18:09:44 -0600

    Even more enemy subtypes
    + Additional class methods
    + Simplified crit_chance probabilities

--- enemy.h
+++ enemy.h
@@ -1,9 +1,22 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 
-enum EnemyType{Ghost, Zombie, Skeleton, Vampire, Ghoul}; // Base enemy subtypes
+enum EnemyType{Snowman, Ghost, Zombie, Skeleton, Vampire, Ghoul, Diary, ZippyFlap}; // Base enemy subtypes
 
 /* Enemy attribute list:
+
+*** DEFAULT ENEMIES ***
+
+struct Snowman { // Tutorial enemy, super trash
+
+    int health = 10;
+    int max_health = 10;
+    int base_attack = 0;
+    double crit_chance = 0.0;
+
+}
+
+*** BASIC ENEMIES ***
 
 struct Ghost { // Basic enemy
 
@@ -26,11 +39,13 @@ struct Zombie { // Basic enemy, tanky
 struct Skeleton { // Basic enemy, glass cannon
 
     int health = 60;
-    int max_health = 75;
+    int max_health = 60;
     int base_attack = 10;
     double crit_chance = 0.3;
 
 };
+
+*** SPECIAL ENEMIES ***
 
 struct Vampire { // Health stealer
 
@@ -46,10 +61,29 @@ struct Ghoul { // More annoying, regenerative variant of Ghost type
     int health = 100;
     int max_health = 100;
     int base_attack = 5;
-    double crit_chance = 0.4
+    double crit_chance = 0.4;
 
 };
 
+*** BOSS ENEMIES ***
+
+struct Diary { // Main boss, the source of everything
+
+    int health = 420;
+    int max_health = 420;
+    int base_attack = 20;
+    double crit_chance = 0.5;
+
+}
+
+struct ZippyFlap { // Miniboss, crit_chance increases gradually
+
+    int health = 200;
+    int max_health = 200;
+    int base_attack = 15;
+    double crit_chance = 0.1;
+
+}
 
 */
 
@@ -59,10 +93,11 @@ class enemy {
 
         enemy(); // Default constructor
         enemy(EnemyType, int, int, int, double); // Enemy type, health, max_health, base_attack, crit_chance
-        enemy(EnemyType); // Set attributes based only on enemy type (easier?)
+        enemy(EnemyType); // Set attributes based on enemy type (easier?)
         ~enemy();
 
         void adjustHealth(int); // For taking damage/healing
+        void adjustCrit(double); // For ZippyFlap enemy
         int attack(); // For dealing damage
         bool getAlive();
 
