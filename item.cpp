81094faaef745674fffb917a6556664ace458058
Globalgig <00alexsouthard@gmail.com>
2019-04-24 20:34:50 -0600

    Add files via upload

--- item.cpp
+++ item.cpp
@@ -1,51 +1,74 @@
 #include "item.h"
 
-Item *ItemPool[30];
+Item *ItemPool[50];
 
 void populateItems() {
     //Trash
-    ItemPool[0] = new Item(0,weapon, "A Dry Stick", true, 5, 0, 0.01, 0);
-    ItemPool[1] = new Item(1,weapon, "A Dry Twig", true, 2, 0, 0.01, 0);
-    ItemPool[2] = new Item(2,weapon, "A Brick", true, 15, 0, 1, 0);
-    ItemPool[3] = new Item(3,weapon, "A Wooden Board", false,  12, 0, 1, 0);
+    ItemPool[0] = new Item(0,weapon, "A Dry Twig", true, 5, 0, 0.01, 0);
+    ItemPool[1] = new Item(1,weapon, "A Rusty Sword", true, 2, 0, 0.01, 0);
+    ItemPool[2] = new Item(2,weapon, "A Brick", true, 15, 0, 0.01, 0);
+    ItemPool[3] = new Item(3,weapon, "A Wooden Board", false,  12, 0, 0.01, 0);
+    ItemPool[4] = new Item(4,weapon, "Candlestick", false, 1, 0, 0.0, 0, "Not for attacking.");
+    ItemPool[5] = new Item(5,weapon, "The Lead Pipe", false, 5, 0, 0.0, 0, "Maybe its a clue. Mustard did it?");
+    ItemPool[6] = new Item(6,weapon, "Wooden Stake", false, 11, 0, 0.0, 0, "Watch out for vampires.");
+
 
     //Consumables
-    ItemPool[4] = new Item(4,consumable, "A Healing Potion", true, 0, 0, 0, 30);
-    ItemPool[5] = new Item(5,consumable, "Potion of Mega Healing", true, 0, 0, 0.0, 50);
-    ItemPool[6] = new Item(6,consumable, "Potion of Iron Skin", true, 0, 5, 0.0, 0);
-    ItemPool[7] = new Item(7,consumable, "Potion of the Beserker", true, 5, 0, 0.0, 0);
-    ItemPool[8] = new Item(8,consumable, "Witches Brew", true, 10, 10, 0.0, -10, "attack and defense at what cost?");
+    ItemPool[7] = new Item(7,consumable, "A Healing Potion", true, 0, 0, 0, 30, "Drink to heal.");
+    ItemPool[8] = new Item(8,consumable, "Potion of Mega Healing", true, 0, 0, 0.0, 50, "Drink to BIG heal.");
+    ItemPool[9] = new Item(9,consumable, "Potion of Iron Skin", true, 0, 5, 0.0, 0, "Increase defence.");
+    ItemPool[10] = new Item(10,consumable, "Potion of the Beserker", true, 5, 0, 0.0, 0,"\"Become strong.\"");
+    ItemPool[11] = new Item(11,consumable, "Witches Brew", true, 10, 10, 0.0, -10, "attack and defense at what cost?");
+    ItemPool[12] = new Item(12,consumable,"Healing Potion of Death", true, 0, 0, 0, -100, "Drink this to heal for -100 healing death, guaranteed");
+    ItemPool[13] = new Item(13,consumable, "Water", true, 0, 0, 0 , 2, "Crisp and refreshing!");
+    ItemPool[14] = new Item(14,consumable, "Bubbly Water", true, 0, 0, 0 , 6, "Ooh bubbly!");
+    ItemPool[15] = new Item(15,consumable, "Pocket Lint", true, 0, 0, 0.0, 0, "Crunchy.");
+    ItemPool[16] = new Item(16,consumable, "Biscuit", true, 0, 0, 0 , 4, "Tastes like it has been there for a little while...");
+    ItemPool[17] = new Item(17,consumable, "Swönky roll", true, 0, 0, 0 , -1, "Patent pending");
+
 
     //Ok Weapons
-    ItemPool[9] = new Item(9,weapon, "A Kitchen Knife", false, 6, 0, .5, 0);
-    ItemPool[10] = new Item(10,weapon, "A Kitchen Plate", true, 15, 0, 0.1, 0);
-    ItemPool[11] = new Item(11,weapon, "A Dining Room Chair", true, 25, 0, 0.1, 0);
-    ItemPool[12] = new Item(12,weapon, "Iron Sword", false, 15, 0, 0.0, 0);
-    ItemPool[13] = new Item(13,weapon, "War Axe", false, 20, 0, 0.0, 0);
+    ItemPool[18] = new Item(18,weapon, "A Kitchen Knife", false, 6, 0, .5, 0);
+    ItemPool[19] = new Item(19,weapon, "A Kitchen Plate", true, 15, 0, 0.1, 0);
+    ItemPool[20] = new Item(20,weapon, "A Dining Room Chair", true, 25, 0, 0.1, 0);
+    ItemPool[21] = new Item(21,weapon, "Iron Sword", false, 15, 0, 0.0, 0);
+    ItemPool[22] = new Item(22,weapon, "War Axe", false, 20, 0, 0.0, 0);
+    ItemPool[23] = new Item(23,weapon, "Giant Hammer", false, 13, 0, 0.13, 0, "A giant could use this!");
 
 
     //Good weapons
-    ItemPool[14] = new Item(14,weapon, "Kingkiller", false, 50, 0, 0.2, 0);
-    ItemPool[15] = new Item(15,weapon, "The Viper", false, 10, 0, 0.75, 0);
+    ItemPool[24] = new Item(24,weapon, "Kingkiller", false, 50, 0, 0.2, 0);
+    ItemPool[25] = new Item(25,weapon, "The Viper", false, 10, 0, 0.75, 0);
+    ItemPool[26] = new Item(26,weapon, "Leech Blade", false, 10, 0, 0.0, 10, "Crafted from the tooth of vampire.");
+    ItemPool[27] = new Item(27,weapon, "Crossbow", false, 15, 0, 0.5, 0, "This could come in handy.");
 
 
     //Armor
-    ItemPool[16] = new Item(16,armor, "Wooden Shield", false, 0, 10, 0.0, 0);
-    ItemPool[17] = new Item(17,armor, "Bucket Helmet", false, 0, 15, 0.0, 0);
-    ItemPool[18] = new Item(18,armor, "Chain Mail", false, 0, 30, 0.0, 0);
-    ItemPool[19] = new Item(19,armor, "Steel Plate Mail", false, 0, 50, 0.0, 0);
-    ItemPool[20] = new Item(20,armor, "Armor of Warding", true, 0, 70, 0.0, 0);
+    ItemPool[28] = new Item(28,armor, "Wooden Shield", false, 0, 10, 0.0, 0);
+    ItemPool[29] = new Item(29,armor, "Bucket Helmet", false, 0, 15, 0.0, 0);
+    ItemPool[30] = new Item(30,armor, "Chain Mail", false, 0, 30, 0.0, 0);
+    ItemPool[31] = new Item(31,armor, "Steel Plate Mail", false, 0, 50, 0.0, 0);
+    ItemPool[32] = new Item(32,armor, "Armor of Warding", true, 0, 70, 0.0, 0);
+    ItemPool[33] = new Item(33,armor, "Imbued Body Plate", false, 0, 18, 0.0, 0, "Glows with a faint blue light");
+    ItemPool[34] = new Item(34,armor, "Paper Tuxedo", false, 0, 1, 0.0, 0, "Lookin’ sharp ;)");
+    ItemPool[35] = new Item(35,armor, "Leather chestpiece", false, 0, 10, 0.0, 0, "Not the best material");
+
 
     //Spells
-    ItemPool[21] = new Item(21,spell, "Fireball", false, 10, 0, 0.0, 0, "Standard spell dealing 10 damage.");
-    ItemPool[22] = new Item(22,spell, "Deadly Dichotomy", false, 75, 0, 0.0, -50, "High damage spell that comes at a price.");
-    ItemPool[23] = new Item(23,spell, "Inferno", false, 30, 0, 0.0, 0, "Hgh damage fireball");
-    ItemPool[24] = new Item(24,spell, "Heal", false, 0, 0, 0.0, 10, "Standard healing spell restores 10 health");
-    ItemPool[25] = new Item(25,spell, "Magic Missle", false, 40, 0, 0.2, 0, "Very high damage spell");
-    ItemPool[26] = new Item(26,spell, "Healing Aura", false, -20, 0, 0.0, 40);
+    ItemPool[36] = new Item(36,spell, "Fireball", false, 10, 0, 0.0, 0, "Standard spell dealing 10 damage.");
+    ItemPool[37] = new Item(37,spell, "Deadly Dichotomy", false, 75, 0, 0.0, -50, "High damage spell that comes at a price.");
+    ItemPool[38] = new Item(38,spell, "Inferno", false, 30, 0, 0.0, 0, "Hgh damage fireball");
+    ItemPool[39] = new Item(39,spell, "Heal", false, 0, 0, 0.0, 10, "Standard healing spell restores 10 health");
+    ItemPool[40] = new Item(40,spell, "Magic Missle", false, 40, 0, 0.2, 0, "Very high damage spell");
+    ItemPool[41] = new Item(41,spell, "Healing Aura", false, -20, 0, 0.0, 40);
+    ItemPool[42] = new Item(42,spell, "Vaporizing Laser", false, 20, 0, 0.33, 0, "ERADICATE");
+    ItemPool[43] = new Item(43,spell, "Scroll of Pebble Rain", true, 4, 0, .5, 0, "Rain pebbles on your foes!");
+    ItemPool[44] = new Item(44,spell, "Very Spontaneous Meteorite", true, 15, 0, .3 , -5, "A giant flaming rock falls out of the sky!");
+
 
     //Scrolls
-    ItemPool[27] = new Item(27,spell, "Scroll of Lighting", true, 40, 0, 0.0, 0, "High damage single use spell");
-    ItemPool[28] = new Item(28,spell, "Scroll of Instadeath", true, 20, 0, 0.0, -1000, "Be carefull with this one");
-    ItemPool[29] = new Item(29,spell, "Scroll of Bladestorm", false, 20, 0, 1.0, 0, "This spell is single use but always crits.");
+    ItemPool[45] = new Item(45,spell, "Scroll of Lighting", true, 40, 0, 0.0, 0, "High damage single use spell");
+    ItemPool[46] = new Item(46,spell, "Scroll of Instadeath", true, 20, 0, 0.0, -1000, "Be carefull with this one");
+    ItemPool[47] = new Item(47,spell, "Scroll of Bladestorm", false, 20, 0, 1.0, 0, "This spell is single use but always crits.");
+    ItemPool[48] = new Item(48,spell, "Scroll of Bladestorm", false, 20, 0, 1.0, 0, "This spell is single use but always crits.");
 }
--- room.cpp
+++ room.cpp
@@ -121,6 +121,7 @@ void room::cleared() {
 
 room::room() {
     srand(time(NULL));
+    int tempprob;
 
     //outside
     outside.description = "You look over your shoulder. Tendrils of fog slither out from the encroaching forest like snakes, hissing as they slide over the dead ground. Menacing shadows watch you from the cover of the trees. The night is frigid; you pull your clothes tighter. If you spend any longer outside, you’ll freeze to death.\n";
@@ -132,11 +133,9 @@ room::room() {
     outside.roomName = "You're outside.\n";
     if(true) {
         outside.monster = outsideSnowman;
-        outside.enemyDes = "A snowman rushes toward you! It has a vicious look in its coal eyes and a wicked grin to sinister mouth. The carrot sticking out of it is twisted.\n";
+        outside.enemyDes = "A snowman rushes toward you! It has a vicious look in its coal eyes and a wicked grin to its sinister mouth. The carrot sticking out of it is twisted.\n";
     }
-    outside.items[0] = ItemPool[2];
-    outside.items[1] = ItemPool[1];
-    outside.items[2] = ItemPool[1];
+    outside.items[0] = ItemPool[1];
 
     //front of house
     frontOfHouse.roomName = "Cautiously, you approach the manor. Loose shingles rattle from the roof above. As you step up the wooden stairs leading to the door, the creaking underfoot sends shivers up your spine. The door, black and glossy, stands before you.\n";
@@ -145,21 +144,19 @@ room::room() {
     frontOfHouse.tryW = "You walk into the fountain. It trips you. You get out and are facing north. You better get indoors soon!\n";
     frontOfHouse.description = "You are standing in front of the hosue.\n";
     frontOfHouse.north = &knocking;
-    if(rand()%7==1) {
-        frontOfHouse.items[0] = ItemPool[3];
-    }
+    frontOfHouse.items[0] = ItemPool[3];
 
     //knocking on door
-    knocking.roomName = "    You knock on the door, sending a dull thud echoing through the inside of the house. After a moment’s pause, a shudder passes through the exterior of the house. You wait, but you don’t hear anyone come to open the door. The air is getting colder; your breath spirals around you.\n";
-    knocking.roomName += "   You reach for the doorknob, but when your hand touches the brass, a loud click comes from within. Soundlessly, the door swings open. The inside of the house is darker than the night, but you can feel the air is a little warmer there.\n";
-    knocking.tryS = "The warm air of the mansion pulls you in. The cold air chills your borns instinctively making you step back towards the door.\n";
-    knocking.tryE = "Porch railing blocks your path that way.\n";
-    knocking.tryW = "There is a loose board and you trip on it. When you stand back up you are facing the door of the mansion.\n";
+    knocking.roomName = "You knock on the door, sending a dull thud echoing through the inside of the house. After a moment’s pause, a shudder passes through the exterior of the house. You wait, but you don’t hear anyone come to open the door. The air is getting colder; your breath spirals around you.\n";
+    knocking.roomName += "You reach for the doorknob, but when your hand touches the brass, a loud click comes from within. Soundlessly, the door swings open. The inside of the house is darker than the night, but you can feel the air is a little warmer there.\n";
+    knocking.tryS = "The warm air of the mansion pulls you in. You shouldn't stay outside.\n";
+    knocking.tryE = "Wooden porch railing blocks that way.\n";
+    knocking.tryW = "You trip on a loose board. When you stand back, up you're facing the door of the mansion.\n";
     knocking.north = &foyer;
-    knocking.items[0] = ItemPool[4];
+    knocking.items[0] = ItemPool[2];
 
     //the foyer
-    foyer.description = "   You step inside, and the sound of the wind immediately dies behind you. Shivering, you let the warmer air thaw the chill set in your bones. There is a lone candle sitting on the floorboards whose light lets you discern the features of the room.";
+    foyer.description = "You step inside, and the sound of the wind immediately dies behind you. Shivering, you let the warmer air thaw the chill set in your bones. There is a lone candle sitting on the floorboards whose light lets you discern the features of the room.";
     foyer.description += "It's a large foyer with doors to the north, east, and west. You look behind you, but strangely the door you came through is gone! An enormous chandelier hangs above, but none of its candles are lit.\n";
     foyer.roomName = "You enter the foyer.\n";
     foyer.visited = false;
@@ -169,29 +166,29 @@ room::room() {
     foyer.tryS = "The wall is made of solid wood; you cannot go this direction.\n";
     if(rand()%4 == 1) {
         foyer.monster = new enemy(Ghost);
-        foyer.enemyDes = "You here a wail and the ghost of a small girl attacks you.\n";
+        foyer.enemyDes = "You hear a wail, and the ghost of a small girl flies toward you!\n";
     }
-    if(true) {
-        foyer.items[0] = ItemPool[(rand()%25)];
-    }
+    foyer.items[0] = ItemPool[4];
+    foyer.items[1] = ItemPool[5];
+    foyer.items[2] = ItemPool[17];
+
 
     //the diningRoom
     diningRoom.description = "You enter a long, unlit dining room. A sturdy oak table spans the length of the room with enough chairs to fit more than twenty people. The table is set with plates, knives, glasses, and forks, but there's no food; however, you can smell something delivious in the air. There are doors leading north, east, and south\n";
     diningRoom.north = &trophyRoom;
     diningRoom.south = &foyer;
     diningRoom.east = &kitchen;
-    diningRoom.tryW = "You try to go West but trip over a dining room chair tumbling majestically several times. When you stand back up you are facing North.";
-    diningRoom.roomName = "You enter the dining room\n";
+    diningRoom.tryW = "You try to go west but trip over a dining room chair and tumble majestically to the floor. When you stand back up, you are facing north.";
+    diningRoom.roomName = "You enter the dining room.\n";
     if(rand()%2 == 1) {
         diningRoom.monster = new enemy(Zombie);
-        diningRoom.enemyDes = "A Zombie gets up from the table, and slowly hobbles over to you.\n";
+        diningRoom.enemyDes = "A Zombie gets up from the table and slowly hobbles over to you.\n";
     }
-    diningRoom.items[0] = ItemPool[5];
-    diningRoom.items[1] = ItemPool[6];
-    diningRoom.items[2] = ItemPool[7];
-    if(rand()%5==1) {
-        diningRoom.items[3] = ItemPool[rand()%25];
-    }
+    diningRoom.items[0] = ItemPool[18]; //knife
+    diningRoom.items[1] = ItemPool[19]; //plate
+    diningRoom.items[2] = ItemPool[20]; //chair
+    diningRoom.items[3] = ItemPool[16];
+
 
     //right hallway
     rightHallway.description = "The hallway you step into is short but has doors leading north, east, south, and west. A painting of an old man hangs on one of the walls. You hear the door behind you creak shut.\n";
@@ -199,7 +196,8 @@ room::room() {
     rightHallway.west = &foyer;
     rightHallway.south = &roomWithCouch;
     rightHallway.tryN = "You try to open the door, but it's locked.\n";
-    rightHallway.roomName = "You enter a small hallway\n";
+    rightHallway.roomName = "You enter a small hallway.\n";
+    rightHallway.items[0] = ItemPool[rand()%3 + 21];
 
     //left hallway
     leftHallway.description = "You open the door to a small hallway. Stepping through, you see doors leading to the north, east, south, and west. A painting of an ancient woman is hanging next to the north door. The hinges of the door quietly scream as they shut.\n";
@@ -208,6 +206,9 @@ room::room() {
     leftHallway.east = &foyer;
     leftHallway.west = &foggyGreenhouse;
     leftHallway.roomName = "You enter a small hallway.\n";
+    leftHallway.items[0] = ItemPool[5];
+    leftHallway.items[1] = ItemPool[rand()%6 + 7];
+
 
     //foggy Greenhouse
     foggyGreenhouse.description = "You step into a greenhouse bustling with dead plants and malignant fungus. Though the walls are made of tinted glass, a thick, wet fog clings to the surface, obscuring your view of the manor’s grounds. Some of the plants look content in their small pots, but a few have spread like a plague out of their containers and over the floorboards all the way to the only door leading East. The air is repugnant here.\n";
@@ -217,23 +218,34 @@ room::room() {
     foggyGreenhouse.tryS = "There is a wall there.\n";
     foggyGreenhouse.roomName = "You enter the foggy greenhouse.\n";
     foggyGreenhouse.monster = new enemy(Ghoul);
-    foggyGreenhouse.enemyDes = "A Ghoul slowly approaches, barely visible through the fog.\n";
+    foggyGreenhouse.enemyDes = "A ghoul slowly approaches, barely visible through the fog.\n";
+    tempprob = rand()%3;
+    for (int i = 0; i < tempprob; i++){
+        foggyGreenhouse.items[i] = ItemPool[rand()%48];
+    }
+
 
     //library
     library.description = "Shelves spilling with books line the walls and fill the room. Long cobwebs hang from the ceiling, their sticky fibers running between the shelves like bridges. You can feel the dust tickle your lungs from the air you breathe in. A hushed creaking comes from behind the shelves where you cannot see.\n";
     library.north = &leftHallway;
-    library.tryS = "There is a bookcase blocking that way.\n";
-    library.tryE = "There is a bookcase a through the cracks you see a shadow moving around.\n";
-    library.tryW = "You trip over a pile of books. When you stand back up, you are facing North.\n";
+    library.tryS = "A bookcase blocks that way.\n";
+    library.tryE = "Through a crack between books, you can see shadows dance.\n";
+    library.tryW = "You trip over a pile of books. When you stand back up, you are facing north.\n";
     library.roomName = "You enter the library.\n";
+    library.items[0] = ItemPool[rand() % 3 + 45];
+    library.items[1] = ItemPool[rand() % 4 + 36];
+    library.items[2] = ItemPool[rand() % 5 + 40];
+
 
     //barren room
     barrenRoom.description = "The ceiling of the room you enter is high and arched with cracked beams barely supporting it. Empty of furniture or windows, your candle’s light is all that reveals the interior of the barren room. Doors leading north and south stand on either side of the room. The floorboards beneath you moan against your weight, breaking the oppressive silence.\n";
     barrenRoom.north = &sittingRoom;
     barrenRoom.south = &leftHallway;
-    barrenRoom.tryE = "There is nothing over there.";
-    barrenRoom.tryW = "You start going that way but fear tripping, so backtrack a litte. While backtracking you trip over backwords.\n";
+    barrenRoom.tryE = "There is nothing over there.\n";
+    barrenRoom.tryW = "You take a tentative step forward, but your fear of tripping causes you to fall backward.\n";
     barrenRoom.roomName = "You enter the barren room.\n";
+    barrenRoom.items[0] = ItemPool[0];
+    barrenRoom.items[1] = ItemPool[8];
 
     //sitting room
     sittingRoom.description = "Whispering fills your ears as you step into the sitting room. A table and set of three dilapidated chairs stand in the center of the room. The sole window is covered by thick, black curtains. Along one of the walls, five portraits of a family hang: a mother, father, daughter, and two sons; their eyes watch you. To the south, east, and west, doors lead.\n";
@@ -243,10 +255,10 @@ room::room() {
     sittingRoom.roomName = "You enter the sitting room.\n";
     if(rand()%3 == 1) {
         sittingRoom.monster = new enemy(Skeleton);
-        sittingRoom.enemyDes = "A Skeleton rattles, still clutching some jewls. When it stands, everything falls through its body. It angrily wobbles in your direction.\n";
+        sittingRoom.enemyDes = "A skeleton stands before you. It hobbles toward you, still clutching some jewels. When it stands, everything falls through its body. It angrily wobbles in your direction.\n";
     }
-    if(rand()%2 == 1) {
-        sittingRoom.items[0] = ItemPool[rand()%25];
+    if(rand()%4 == 1) {
+        sittingRoom.items[0] = ItemPool[rand()%4 + 24];
     }
 
     //trophy room
@@ -254,33 +266,57 @@ room::room() {
     trophyRoom.south = &diningRoom;
     trophyRoom.west = &sittingRoom;
     trophyRoom.roomName = "You enter the trophy room.\n";
+    trophyRoom.items[0] = ItemPool[rand()%8 + 28];
+    trophyRoom.items[1] = ItemPool[42];
+    trophyRoom.items[2] = ItemPool[7];
+    if (rand()%10 == 1) {
+        trophyRoom.items[3] = ItemPool[rand()%4+24];
+    }
+
 
     //kitchen
     kitchen.description = "A clatter splits the air as you step into the kitchen. You see a door swiftly close, then the room returns to silence. On the floor lays a bloody knife, the source of the noise. An array of pantries wrap around the room, and a large island rests in the center. You can see a wooden cutting board on the island drenched in blood. Doors lead to the north and west.\n";
     kitchen.north = &cellar;
     kitchen.west = &diningRoom;
     kitchen.roomName = "You enter the kitchen.\n";
-
+    kitchen.items[0] = ItemPool[18];
+    kitchen.items[1] = ItemPool[18];
+    kitchen.items[2] = ItemPool[7];
+    if (rand()%10 == 1) {
+        kitchen.items[3] = ItemPool[27];
+    }
     //cellar
     cellar.description = "You feel the dank air on your skin as you step into the cellar. Barrels are stacked throughout the room, rotten and cracked. Water drips from the ceiling above and hits the shallow puddle along the floor with a splash. The only door is from the way you came: south.\n";
     cellar.south = &kitchen;
     cellar.roomName = "You enter the cellar.\n";
     if(rand()%2==1) {
         cellar.monster = new enemy(Vampire);
-        cellar.enemyDes = "A Vampire rises from its coffin and attacks you. Going for your kneck";
-        cellar.items[0] = ItemPool[rand()%25];
-        cellar.items[1] = ItemPool[rand()%25];
+        cellar.enemyDes = "A vampire rises from its coffin and goes for your neck!\n";
+        cellar.items[1] = ItemPool[rand()%4 + 24];
+        cellar.items[2] = ItemPool[rand()%8 + 28];
     }
+    cellar.items[0] = ItemPool[rand()%11 + 7];
 
     //room with couch
     roomWithCouch.description = "A long couch, cleaved in two and splattered with dried blood, is the only furniture in an otherwise empty room. You can see red eyes glint through the window on the opposite wall. Creaking comes through the ceiling above. The only door is to the north.\n";
     roomWithCouch.north = &rightHallway;
     roomWithCouch.roomName = "You enter the couch room.\n";
+    roomWithCouch.monster = new enemy(Ghost, 113, 113, 7, 0.3);
+    roomWithCouch.enemyDes = "Oh sh*t a ghost girl crawls out of the couch and kills your head off!\n";
+    roomWithCouch.items[0] = ItemPool[rand()%3 + 21];
+    roomWithCouch.items[1] = ItemPool[17];
+    roomWithCouch.items[2] = ItemPool[rand()%3 + 45];
+    roomWithCouch.items[3] = ItemPool[7];
 
     //piano room
     pianoRoom.description = "As you step into the room, you spy a decaying grand piano. Faint as a breath, quiet notes suffuse the air, their eerie tune making your hair stand on end. You can see nothing striking the keys to make the music. A door leads north.\n";
     pianoRoom.north = &office;
     pianoRoom.roomName = "You enter the piano room.\n";
+    tempprob = rand()%101;
+    if (rand()%4 == 1) {
+        pianoRoom.monster = new enemy(Skeleton, tempprob, tempprob, rand()%21, 0.01);
+        pianoRoom.enemyDes = "The jangling of bones approaches! A skeleton attacks!";
+    }
 
     //office
     office.description = "The office you enter is perfectly in order. A large desk is stacked neatly with letters and papers, the contents of which are written in an unintelligible script. What sounds like scratching comes from within the walls. Doors lead to the west, north, and south.\n";
@@ -288,18 +324,23 @@ room::room() {
     office.west = &rightHallway;
     office.south = &roomWithCouch;
     office.roomName = "You enter the office.\n";
-    if(rand()%4==1) {
-        office.items[0] = ItemPool[rand()%25];
+    tempprob = rand()%3;
+    for (int i = 0; i < tempprob; i++) {
+        office.items[i + 1] = ItemPool[rand()%7];
     }
+    office.items[0] = ItemPool[rand()%3 + 21];
 
     //bathroom 1
     bathroom1.description = "You enter a bathroom made of white marble. The mirror, sink, and toilet are all cracked and crumbling. The sole door is to the south. An ominous gurgling comes from inside the toilet basin.\n";
     bathroom1.south = &office;
     bathroom1.roomName = "You enter the white bathroom.\n";
-    if(rand()%2 == 1) {
+    tempprob = rand()%5;
+    if(tempprob != 4) {
         bathroom1.monster = new enemy(ZippyFlap);
-        bathroom1.enemyDes = "A ZippyFlap was lying in wait. It attacks you.";
-        bathroom1.items[0] = ItemPool[rand()%25];
+        bathroom1.enemyDes = "A zippyflap was lying in wait. It attacks you!\n";
+        bathroom1.items[0] = ItemPool[43];
+        bathroom1.items[1] = ItemPool[rand()%4 + 37];
+        bathroom1.items[2] = ItemPool[rand()%3 + 28];
     }
 
     //bathroom 2
@@ -307,7 +348,8 @@ room::room() {
     bathroom2.east = &sittingRoom;
     bathroom2.roomName = "You enter the dark bathroom.\n";
     bathroom2.monster = new enemy(Diary);
-    bathroom2.enemyDes = "The final boss, A Diary attacks you.";
+    bathroom2.enemyDes = "The final boss, a diary attacks you.";
+    //haha you'll never beat it lmao
 }
 
 QString room::saveGame() {
@@ -448,6 +490,7 @@ QString room::loadGame(QString load) {
     if(load[17] == "1")
         currentRoom = &bathroom2;
     if(load[18] == "1")
+        currentRoom = &foggyGreenhouse;
     return currentRoom->roomName;
 }
 
